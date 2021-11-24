4c09fb9bc3c9b231a3ef13ac0ed1befa5cf59a7e
Noah Sherwin <noahrsherwin@gmail.com>
2021-11-24 14:59:23 -0800

    refactor: remove built-in Get-VSInstallDir; rely on external module
    BREAKING CHANGE

--- src/Enter-VSDevEnv.psm1
+++ src/Enter-VSDevEnv.psm1
@@ -1,28 +1,16 @@
-function Get-VSInstallPath {
-    [CmdletBinding()] param (
-        [Parameter()] [bool] $Prerelease = $true
-    )
-
-    Write-Verbose -Message "`$Prerelease == $Prerelease"
-
-    if ($Prerelease) {
-        Write-Verbose -Message '"${env:ProgramFiles(x86)}/Microsoft Visual Studio/Installer/vswhere.exe" -prerelease -latest -property installationPath;'
-        return & "${env:ProgramFiles(x86)}/Microsoft Visual Studio/Installer/vswhere.exe" -prerelease -latest -property installationPath;
-    }
-    else {
-        Write-Verbose -Message '"${env:ProgramFiles(x86)}/Microsoft Visual Studio/Installer/vswhere.exe" -latest -property installationPath;'
-        return & "${env:ProgramFiles(x86)}/Microsoft Visual Studio/Installer/vswhere.exe" -latest -property installationPath;
-    }
-}
-
 function Enter-VSDevEnv {
     [CmdletBinding()] param (
         [Parameter()] [bool] $Prerelease = $true
     )
 
-    $vsInstallPath = Get-vsInstallPath;
-    Write-Verbose -Message "`$vsInstallPath == $vsInstallPath"
+    try {
+        $vsInstallDir = Get-vsInstallDir;
+    }
+    catch {
+        Import-Module "$PSScriptRoot\..\deps\Posh.Get-VSInstallDir\src\Get-VSInstallDir.psm1"
+        $vsInstallDir = Get-vsInstallDir;
+    }
 
-    Import-Module "$vsInstallPath/Common7/Tools/Microsoft.VisualStudio.DevShell.dll";
-    Enter-VsDevShell -VsInstallPath $vsInstallPath -SkipAutomaticLocation;
+    Import-Module "$vsInstallDir/Common7/Tools/Microsoft.VisualStudio.DevShell.dll";
+    Enter-VsDevShell -VsInstallDir $vsInstallDir -SkipAutomaticLocation;
 }
